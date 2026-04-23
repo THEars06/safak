3247dc8e8dca571b7b89bb67d1558e124270ffd3
Mehmet Ali Arslan <117948751+THEars06@users.noreply.github.com>
2026-04-23 21:35:51 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.quantumroyal.com/';
+const API_URL = 'https://api.quantumroyal.com/api';
 
 export async function login(email: string, password: string) {
   const response = await fetch(`${API_URL}/login`, {
